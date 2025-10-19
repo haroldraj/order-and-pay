ca5960a47ae1e68f8b37ea7c9a3540d7e572abeb
Harold Rajaonarison <rnarsonharold@gmail.com>
2025-10-19 23:36:07 +0200

    migrate to microservice architecture

---
+++ user-serice/src/test/java/com/hrajaona/userserice/UserSericeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hrajaona.userserice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserSericeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
