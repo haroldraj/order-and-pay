6699188a5fc50173736faa22d67f439d55a4569d
Harold Rajaonarison <rnarsonharold@gmail.com>
2025-10-21 14:22:26 +0200

    add user initial tables

---
+++ user_service/src/main/resources/db/migration/V1__baseline.sql
@@ -0,0 +1,35 @@
+CREATE TABLE IF NOT EXISTS users(
+    id BIGSERIAL PRIMARY KEY ,
+    user_idf VARCHAR(36) NOT NULL,
+    user_name VARCHAR(50) NOT NULL,
+    phone_number VARCHAR(25) NOT NULL,
+    email_address VARCHAR(30) NOT NULL UNIQUE,
+    status VARCHAR(30),
+    created_at TIMESTAMP DEFAULT NOW()
+    );
+
+CREATE TABLE IF NOT EXISTS addresses(
+    id BIGSERIAL PRIMARY KEY ,
+    address_idf VARCHAR(36) NOT NULL,
+    user_id BIGINT REFERENCES users(id) ON DELETE CASCADE,
+    label VARCHAR(50),
+    street VARCHAR(50) NOT NULL,
+    house_number VARCHAR(50) NOT NULL,
+    box_number INTEGER,
+    city VARCHAR(30),
+    postal_code INTEGER,
+    country VARCHAR(30),
+    is_default BOOLEAN,
+    created_at TIMESTAMP DEFAULT NOW()
+    );
+
+CREATE TABLE IF NOT EXISTS roles(
+  id BIGSERIAL PRIMARY KEY ,
+  role_name VARCHAR(50) UNIQUE NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS user_roles(
+    user_id BIGINT REFERENCES users(id) ON DELETE CASCADE,
+    role_id BIGINT REFERENCES roles(id) ON DELETE CASCADE,
+    PRIMARY KEY (user_id, role_id)
+);
